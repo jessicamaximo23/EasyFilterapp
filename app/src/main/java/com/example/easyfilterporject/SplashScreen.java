347b8c0bbf1f4d47feb5fc3cef570805d6729bf4
jessica maximo <jessicamaximo23@gmail.com>
2024-10-28 10:40:48 -0400

    Login In xml

--- app/src/main/java/com/example/easyfilterporject/SplashScreen.java
+++ app/src/main/java/com/example/easyfilterporject/SplashScreen.java
@@ -9,18 +9,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreen extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        // Temporizador para a splash screen
         new Handler(Looper.getMainLooper()).postDelayed(() -> {
-            // Navega para a MainActivity após 2 segundos
+
             Intent intent = new Intent(SplashScreen.this, MainActivity.class);
             startActivity(intent);
             finish();
-        }, 2000); // Tempo de exibição da splash screen (2 segundos)
+        }, 2000);
     }
 }
